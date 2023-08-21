4af25b626aae28dbdf2620efacf23a370995e221
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-08-22 00:15:20 +0100

    Navbar Added!

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+
+
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">Go Tutorial</div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/tutorial">Tutorial</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
+
+
